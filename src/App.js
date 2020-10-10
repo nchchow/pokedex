7f329999783382f71082bf2de2cad1ead19ccb6d
nelson chow <nelsonchow91.nc@gmail.com>
2020-10-09 21:15:08 -0700

    Implement form for pokemon search term input and submit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { getPokemon } from "./api/pokeapi";
 
 const App = () => {
-  return <div className="App"></div>;
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getPokemon(searchTerm);
+  };
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Search" onChange={handleChange} />
+        <button>Search</button>
+      </form>
+    </div>
+  );
 };
 
 export default App;
