76061de8731b0e874be67d38f4337b96f45fbc36
nelson chow <nelsonchow91.nc@gmail.com>
2020-10-11 11:54:43 -0700

    Use material ui's textfield as input

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
 import Card from "./components/Card";
 import { getPokemon } from "./api/pokeapi";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(2),
+      width: "50vw",
+    },
+  },
+}));
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [pokemon, setPokemon] = useState(null);
+
+  const classes = useStyles();
 
   const handleChange = (e) => {
     setSearchTerm(e.target.value);
@@ -19,9 +32,13 @@ const App = () => {
 
   return (
     <div className="App">
-      <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Search" onChange={handleChange} />
-        <button>Search</button>
+      <form className={classes.root} onSubmit={handleSubmit}>
+        <TextField
+          id="standard-basic"
+          type="text"
+          label="Search Pokemon"
+          onChange={handleChange}
+        />
       </form>
       {pokemon && <Card pokemon={pokemon} />}
     </div>
