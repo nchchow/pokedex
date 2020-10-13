d00b0b3960dc63078843672a6da25de4d4e28131
nelson chow <nelsonchow91.nc@gmail.com>
2020-10-13 11:16:21 -0700

    Add back label to minipokeball

--- src/components/MiniPokeball.js
+++ src/components/MiniPokeball.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const MiniPokeball = ({ handleClose }) => {
   return (
-    <div className="container--mini" onClick={handleClose}>
+    <div className="container--mini" onClick={handleClose} title="Back">
       <div className="pokeball pokeball--mini">
         <div className="pokeball__top pokeball__top--mini">
           <div className="pokeball__button pokeball__button--mini"></div>
